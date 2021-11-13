2b577627e528f3ae3dff86045a8bad9ffffed402
Octavius Bowman <octaviusbowman@gmail.com>
2021-11-13 18:23:40 -0500

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Octavius Bowman on 11/13/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
